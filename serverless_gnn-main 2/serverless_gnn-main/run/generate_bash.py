8fe4d6041f6ee1b3e83815877c6e4b5d3ec8ddd9
TiaHu <112565589+TiaHu@users.noreply.github.com>
2023-02-16 09:25:54 -0500

    Update generate_bash.py

--- serverless_gnn-main 2/serverless_gnn-main/run/generate_bash.py
+++ serverless_gnn-main 2/serverless_gnn-main/run/generate_bash.py
@@ -1,4 +1,4 @@
-total_client = 7
+total_client = 4
 interval = 10
 num_machine = 1
 servr_ip = '127.0.0.1'
@@ -11,7 +11,7 @@ for machine in range(1, num_machine+1):
 	all_files[machine].write('export PYTHONUNBUFFERED=yes\n')
 
 for i in range(1, total_client):
-	all_files[i//client_per_machine + 1].write('python3 -u main_dist.py --rank ' + str(i) + ' --num_parts ' + str(total_client) + ' --interval ' + str(interval) \
+	all_files[i//client_per_machine + 1].write('python3 -u run/main_dist.py --rank ' + str(i) + ' --num_parts ' + str(total_client) + ' --interval ' + str(interval) \
 	+ ' --servr_ip ' + servr_ip
 	+ ' > ' + str(i) + '.log  &\n')
 
